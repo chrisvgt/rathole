b9b788129bbc507e08a044994ef788e75999dff7
chrisvgt <30186211+chrisvgt@users.noreply.github.com>
2026-04-07 21:37:12 +0200

    fix: simplify genkey invocation in run function

--- src/lib.rs
+++ src/lib.rs
@@ -60,8 +60,8 @@ fn genkey(curve: Option<KeypairType>) -> Result<()> {
 }
 
 pub async fn run(args: Cli, shutdown_rx: broadcast::Receiver<bool>) -> Result<()> {
-    if args.genkey.is_some() {
-        return genkey(args.genkey.unwrap());
+    if let Some(value) = args.genkey {
+        return genkey(value);
     }
 
     // Raise `nofile` limit on linux and mac
